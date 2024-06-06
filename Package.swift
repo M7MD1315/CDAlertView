9c235d61dc94a4d46ae15a37091b6e2b94bc3fe8
Mohamed Elmezughi <79088999+M7MD1315@users.noreply.github.com>
2024-06-06 13:50:51 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,19 +1,32 @@
-// swift-tools-version:5.8
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.3
 import PackageDescription
 
-let package =   Package(
-  name: "CDAlertView",
-  products: [
-    .library(name: "CDAlertView", targets: ["CDAlertView"]),
-  ],
-  targets: [
-    .target(name: "CDAlertView", path: "CDAlertView"),
-    .testTarget(
-      name: "CDAlertViewTests",
-      dependencies: ["CDAlertView"],
-      path: "CDAlertViewTests"
-    )
-  ]
+let package = Package(
+    name: "CDAlertView",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "CDAlertView",
+            targets: ["CDAlertView"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "CDAlertView",
+            dependencies: [],
+            path: "CDAlertView",
+            exclude: ["Info.plist"],
+            resources: [
+                .process("Resources/Assets")
+            ]
+        ),
+        .testTarget(
+            name: "CDAlertViewTests",
+            dependencies: ["CDAlertView"],
+            path: "CDAlertViewTests",
+            exclude: ["Info.plist"]
+        ),
+    ]
 )
